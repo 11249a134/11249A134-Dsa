9a0b594c73dbb3c5769d4516bcc2195e90f6f80e
11249a134 <11249a134@kanchiuniv.ac.in>
2026-04-20 21:48:36 +0530

    Update program2.c

--- DAA/program2.c
+++ DAA/program2.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
-int main ()
+int main()
 {
-	int n,i;
-	int a=0,b=1,next;
-	printf("enter number of terms: ");
-	scanf("%d",&n);
-	printf("fibonacci series:\n");
-	for (i=1;i<=n;i++){
-		printf("%d",a);
-		next=a+b;
-		a=b;
-		b=next;
-	}
-	return 0;
+    int n;
+    int a=0,b=1,c;
+    printf(" enther the number of terms(n): ");
+    if(scanf("%d",&n)!=1|| n<=0)
+    {
+    printf("it is invalid plase enter a valid integer");
+    return 0;
+}
+
+printf("Fibonacci series:");
+if (n >= 1)
+  printf("%d",a);
+if (n >= 2)
+  printf("%d",b);
+ for(int i = 3;i <= n; i++)
+{
+    c=a + b;
+    printf("%d ",c);
+    a = b;
+    b = c;
+}
+if (n == 1)
+     printf("\n fibonacci term %d = %d\n", n, a);
+else
+     printf("\nfibonacci term %d = %d \n", n, b);
+return 0;
 }
